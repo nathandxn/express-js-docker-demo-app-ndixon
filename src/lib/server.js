23c4ad91861370e760b880fc87b83a82c17bc2d2
nathandxn <nathan.dixon@snyk.io>
2024-10-14 15:47:13 -0400

    feat: add main get path

--- src/lib/server.js
+++ src/lib/server.js
@@ -9,4 +9,7 @@ app.use(bodyParser.urlencoded({
     extended: true
   }));
 app.use(bodyParser.json());
-  
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
