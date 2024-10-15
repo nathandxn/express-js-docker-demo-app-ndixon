3717644bf6bb7141cefa033625685cb875857785
nathandxn <nathan.dixon@snyk.io>
2024-10-14 20:27:22 -0400

    feat: update rate limiter

--- src/lib/server.js
+++ src/lib/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path'); 
 const RateLimit = require('express-rate-limit');
-const limiter = new RateLimit({
+const limiter = RateLimit({
   windowMs: parseInt(process.env.WINDOW_MS, 10),
   max: parseInt(process.env.MAX_IP_REQUESTS, 10),
   delayMs:parseInt(process.env.DELAY_MS, 10),
@@ -10,11 +10,12 @@ const limiter = new RateLimit({
 
 const port = process.env.PORT || 3000;
 
-app.use(limiter);
 const fs = require('fs'); // file system
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
+
+app.use(limiter);
 
 app.use(bodyParser.urlencoded({
     extended: true
